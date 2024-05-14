6b2d0454872c84d2ce021f6f21dcf6f39b9eab2d
nunupy345345 <nunupy345345@airi9ri>
2024-05-14 09:40:17 +0900

    fix readme

--- app/src/main.py
+++ app/src/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from services.test import Test
 from models import TestModel
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def hello_world():
